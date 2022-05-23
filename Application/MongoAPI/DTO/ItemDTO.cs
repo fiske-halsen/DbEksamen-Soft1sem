9ceaee320026d933e93778012aa8127e058b9072
LAPTOP-69AO34FQ\lukas <lukasbangstoltz@gmail.com>
2022-05-23 22:30:01 +0200

    first commit for neo api

--- Application/MongoAPI/DTO/ItemDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MongoAPI.DTO
-{
-    public class ItemDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Price { get; set; }
-    }
-}
