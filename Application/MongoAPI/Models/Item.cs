c4638a11603dba1d9ba9255be89bab744368d86b
Sebbergo <cph-sh497@cphbusiness.dk>
2022-05-23 16:06:14 +0200

    mongoAPI push first time

---
+++ Application/MongoAPI/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoAPI.Models
+{
+    public class Item
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; } = null!;
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Application/MongoAPI/Services/OrderService.cs
@@ -0,0 +1,12 @@
+﻿namespace MongoAPI.Services
+{
+    public interface IOrderService
+    {
+        public Task<IEnumerable<OrderDTO>> GetAllOrders();
+        public Task<OrderDTO> GetOrder(int orderId);
+        public Task<OrderDTO> CreateOrder(OrderDTO orderDTO);
+    }
+    public class OrderService
+    {
+    }
+}
